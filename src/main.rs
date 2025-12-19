f765346fd2972461753293b429c94289e36b6c6a
blakeminix <blakeminix2@gmail.com>
2025-12-19 14:19:32 -0500

    fibonacci

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,40 @@ fn main() {
     // println!("Hello, world!");
 
     let source = r#"
-        mov r0, 5
-        push r0
-        call double
-        print r2
+        mov r0, 10
+        call fib
+        print r0
         halt
 
-    double:
-        loadr r1, FP, 8
-        add r2, r1, r1
+    fib:
+        mov r1, 0
+        sub r3, r0, r1
+        jz ret_case
+
+        mov r1, 1
+        sub r3, r0, r1
+        jz ret_case
+
+        push r0
+        
+        mov r1, 1
+        sub r0, r0, r1
+        call fib
+        
+        movr r4, r0
+        pop r0
+        
+        push r4
+        
+        mov r1, 2
+        sub r0, r0, r1
+        call fib
+        
+        pop r4
+        add r0, r0, r4
+        ret
+
+    ret_case:
         ret
         "#;
 
