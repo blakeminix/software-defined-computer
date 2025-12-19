3345ba50a011465099c4e2ca67cde7f95a4f6cef
blakeminix <blakeminix2@gmail.com>
2025-12-19 00:13:41 -0500

    fde cycle

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,5 @@
+use crate::instruction::Opcode;
+
 pub const MEMORY_SIZE: usize = 65_536; // 64KB
 
 pub struct CPU {
@@ -33,5 +35,42 @@ impl CPU {
         let byte = self.read_u8(self.pc);
         self.pc = self.pc.wrapping_add(1);
         byte
+    }
+
+    pub fn step(&mut self) {
+        let opcode_byte = self.fetch_u8();
+        let opcode = Opcode::from_byte(opcode_byte).expect("Invalid opcode");
+        
+        match opcode {
+            Opcode::Mov => {
+                let dest = self.fetch_u8() as usize;
+                let imm = self.fetch_u8() as u32;
+
+                self.registers[dest] = imm;
+                self.z = imm == 0;
+            }
+
+            Opcode::Add => {
+                let dest = self.fetch_u8() as usize;
+                let src1 = self.fetch_u8() as usize;
+                let src2 = self.fetch_u8() as usize;
+
+                let result = self.registers[src1].wrapping_add(self.registers[src2]);
+
+                self.registers[dest] = result;
+                self.z = result == 0;
+            }
+
+            Opcode::Print => {
+                let src = self.fetch_u8() as usize;
+                println!("{}", self.registers[src]);
+            }
+
+            Opcode::Halt => {
+                self.halted = true;
+            }
+
+            _ => unimplemented!(),
+        }
     }
 }
